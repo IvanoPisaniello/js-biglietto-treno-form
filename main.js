2270a1bef0648e260a9c4777e4d012e7ace2caba
IvanoPisaniello <ivanop92@gmail.com>
2023-07-03 17:51:28 +0200

    added js and layout

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
 const btnLogin = document.querySelector(".pulsante-genera");
 const inputKmElement = document.querySelector("[name='km']");
 const inputUserAge = document.querySelector("[name='age']");
-
+const inputName = document.querySelector("[name='inputName']");
+const btnReset = document.querySelector(".pulsante-reset");
 
 
 
@@ -14,20 +15,36 @@ btnLogin.addEventListener("click", function () {
     const discount65 = ((prezzoFinale * 40) / 100);
     const totale18 = (prezzoFinale - discount18);
     const totale65 = (prezzoFinale - discount65);
+    const insertName = inputName.value;
+    const min = 1;
+    const max = 10;
+    const finalNum = Math.floor(Math.random() * (max - min + 1)) + min;
+    const numRandom = Math.random();
+
+
 
     if (ageUser <= 18) {
-        console.log(Math.round((discount18 + Number.EPSILON) * 100) / 100) + "€";
-        console.log(Math.round((totale18 + Number.EPSILON) * 100) / 100) + "€";
-        console.log("Complimenti! hai uno sconto del 20% riservato ai minorenni!")
+        document.getElementById("ticketType").innerHTML = ("Sonto del 20%");
+        document.getElementById("prezzo-finale").innerHTML = (Math.round((totale18 + Number.EPSILON) * 100) / 100) + "€";
+        document.getElementById("carrozza").innerHTML = (`Numero tra ${min} e ${max}`, finalNum);
+        document.getElementById("userName").innerHTML = insertName;
+        document.getElementById("code").innerHTML = (Math.round(numRandom * 5000));
     }
     else if (ageUser >= 65) {
-        console.log(Math.round((discount65 + Number.EPSILON) * 100) / 100) + "€";
-        console.log(Math.round((totale65 + Number.EPSILON) * 100) / 100) + "€";
-        console.log("Complimenti! hai uno sconto del 40% riservato agli over 65 anni!");
+        document.getElementById("ticketType").innerHTML = ("sconto del 65%");
+        document.getElementById("prezzo-finale").innerHTML = (Math.round((totale65 + Number.EPSILON) * 100) / 100) + "€";
+        document.getElementById("userName").innerHTML = insertName;
+        document.getElementById("code").innerHTML = (Math.round(numRandom * 5000));
+        document.getElementById("carrozza").innerHTML = (`Numero tra ${min} e ${max}`, finalNum);
+        // console.log("Complimenti! hai uno sconto del 40% riservato agli over 65 anni!");
     }
 
     else {
-        console.log("ci dispiace non c'è uno sconto disponibile per te");
-        console.log(Math.round((prezzoFinale + Number.EPSILON) * 100) / 100) + "€";
+        document.getElementById("ticketType").innerHTML = ("Nessun'offerta disponibile");
+        document.getElementById("prezzo-finale").innerHTML = (Math.round((prezzoFinale + Number.EPSILON) * 100) / 100) + "€";
+        document.getElementById("userName").innerHTML = insertName;
+        document.getElementById("code").innerHTML = (Math.round(numRandom * 5000));
+        document.getElementById("carrozza").innerHTML = (`Numero tra ${min} e ${max}`, finalNum);
     }
 });
+
